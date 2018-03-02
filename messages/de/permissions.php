b77a5d36e0473784f7e2cdc0debc2f3c90219b4b
staxDB <borndavid508@gmail.com>
2018-03-02 16:54:12 +0100

    changed icon

--- messages/de/permissions.php
+++ messages/de/permissions.php
@@ -2,5 +2,4 @@
 return array (
     'View your bookmark stream' => 'Deinen Lesezeichen-Stream sehen',
     'Allow other users to see your bookmarks' => 'Erlaube anderen Nutzern deine Lesezeichen zu sehen'
-
 );
