e6b548dad1fc1eae988fa3e0185967f2e499c615
Sarah Tsumayoi <Felli@users.noreply.github.com>
2018-08-29 14:27:32 +0900

    Update config.php

--- config.php
+++ config.php
@@ -13,12 +13,12 @@ return [
     'class' => 'humhub\modules\bookmark\Module',
     'namespace' => 'humhub\modules\bookmark',
     'events' => [
-        ['class' => ProfileMenu::className(), 'event' => ProfileMenu::EVENT_INIT, 'callback' => ['humhub\modules\bookmark\Events', 'onProfileMenuInit']],
-        ['class' => User::className(), 'event' => User::EVENT_BEFORE_DELETE, 'callback' => ['humhub\modules\bookmark\Events', 'onUserDelete']],
-        ['class' => ActiveRecord::className(), 'event' => ActiveRecord::EVENT_BEFORE_DELETE, 'callback' => ['humhub\modules\bookmark\Events', 'onActiveRecordDelete']],
-        ['class' => IntegrityController::className(), 'event' => IntegrityController::EVENT_ON_RUN, 'callback' => ['humhub\modules\bookmark\Events', 'onIntegrityCheck']],
-        ['class' => WallEntryLinks::className(), 'event' => WallEntryLinks::EVENT_INIT, 'callback' => ['humhub\modules\bookmark\Events', 'onWallEntryLinksInit']],
-        ['class' => TopMenu::className(), 'event' => TopMenu::EVENT_INIT, 'callback' => ['\humhub\modules\bookmark\Events', 'onTopMenuInit']],
+        ['class' => ProfileMenu::class, 'event' => ProfileMenu::EVENT_INIT, 'callback' => ['humhub\modules\bookmark\Events', 'onProfileMenuInit']],
+        ['class' => User::class, 'event' => User::EVENT_BEFORE_DELETE, 'callback' => ['humhub\modules\bookmark\Events', 'onUserDelete']],
+        ['class' => ActiveRecord::class, 'event' => ActiveRecord::EVENT_BEFORE_DELETE, 'callback' => ['humhub\modules\bookmark\Events', 'onActiveRecordDelete']],
+        ['class' => IntegrityController::class, 'event' => IntegrityController::EVENT_ON_RUN, 'callback' => ['humhub\modules\bookmark\Events', 'onIntegrityCheck']],
+        ['class' => WallEntryLinks::class, 'event' => WallEntryLinks::EVENT_INIT, 'callback' => ['humhub\modules\bookmark\Events', 'onWallEntryLinksInit']],
+        ['class' => TopMenu::class, 'event' => TopMenu::EVENT_INIT, 'callback' => ['\humhub\modules\bookmark\Events', 'onTopMenuInit']],
     ],
 ];
 ?>
